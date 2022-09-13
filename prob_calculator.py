b2d86f7fc79e13063e0e516a5f5ce6ed0da64c33
pattisoj <joshpattison23@icloud.com>
2022-09-13 13:18:24 +0100

    3/3 tests - Challenge Completed

--- prob_calculator.py
+++ prob_calculator.py
@@ -3,22 +3,46 @@ import random
 # Consider using the modules imported above.
 
 class Hat:
-    def __init__(self, **kwargs):
-        self.contents = []
-        for k, v in kwargs.items():
-            self.contents += [k] * int(v)
+  def __init__(self, **kwargs):
+    self.contents = []
+    for k, v in kwargs.items():
+      self.contents += [k] * int(v)
 
-    def draw(self, number):
-        if number >= len(self.contents):
-            return self.contents
+  def draw(self, number):
+    n = len(self.contents)
+    if number >= n:
+      return self.contents
       
-        result = []
-        for i in range(0, number):
-            chosen_ball = random.randint(0, n - 1)
-            result.append(self.contents.pop(chosen_ball))
-            n -= 1
+    result = []
+    for i in range(0, number):
+      chosen_ball = random.randint(0, n - 1)
+      result.append(self.contents.pop(chosen_ball))
+      n -= 1
 
-        return result
+    return result
+    
+    
 
 
-# def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+    count = 0
+    for _ in range(0, num_experiments):
+        hat_copy = copy.deepcopy(hat)
+        balls = hat_copy.draw(num_balls_drawn)
+        balls_dict = dict()
+        for ball in balls:
+            balls_dict[ball] = balls_dict.get(ball, 0) + 1
+
+        successful = True
+        for ball, v in expected_balls.items():
+            if balls_dict.get(ball, 0) < v:
+                successful = False
+                break
+
+        if successful:
+            count += 1
+
+    return count / num_experiments
+
+        
+
