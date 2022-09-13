9be7ca50bf462cc2d1b68ae4a72e93ad8455302d
pattisoj <joshpattison23@icloud.com>
2022-09-13 12:56:58 +0100

    1/3 tests - Hat class created

--- prob_calculator.py
+++ prob_calculator.py
@@ -3,6 +3,22 @@ import random
 # Consider using the modules imported above.
 
 class Hat:
+    def __init__(self, **kwargs):
+        self.contents = []
+        for k, v in kwargs.items():
+            self.contents += [k] * int(v)
+
+    def draw(self, number):
+        if number >= len(self.contents):
+            return self.contents
+      
+        result = []
+        for i in range(0, number):
+            chosen_ball = random.randint(0, n - 1)
+            result.append(self.contents.pop(chosen_ball))
+            n -= 1
+
+        return result
 
 
-def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+# def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
